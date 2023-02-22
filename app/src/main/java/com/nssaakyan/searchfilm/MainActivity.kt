012286f94a4f3b1bc3962edd50c440f8d09c4588
Nadezhda Saakyan <nssaakyan@gmail.com>
2023-02-22 13:01:35 +0300

    Module 25. Fragments. Don't work, please help! Version 22.02.2023

--- app/src/main/java/com/nssaakyan/searchfilm/MainActivity.kt
+++ app/src/main/java/com/nssaakyan/searchfilm/MainActivity.kt
@@ -1,52 +1,69 @@
 package com.nssaakyan.searchfilm
 
-import android.annotation.SuppressLint
-import android.os.Build
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import android.widget.Toast
-import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
+
 class MainActivity : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId")
-    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Анимация Лаванда
+        val animationImage: Animation = AnimationUtils.loadAnimation(this, R.anim.lavender_anim)
+        val animationText: Animation = AnimationUtils.loadAnimation(this, R.anim.text_anim)
+        image.startAnimation(animationImage)
+        textView.startAnimation(animationText)
 
-        topAppBar.setNavigationOnClickListener {
-            Toast.makeText(this, "Меню", Toast.LENGTH_SHORT).show()
-        }
-
-        topAppBar.setOnMenuItemClickListener {
-            when (it.itemId) {
-                R.id.settings -> {
-                    Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
-                    true
-                }
-                else -> false
-            }
+        image.setOnClickListener {
+            Toast.makeText(this, R.string.good_day, Toast.LENGTH_SHORT).show()
         }
 
         bottom_navigation.setOnItemSelectedListener() {
 
             when (it.itemId) {
                 R.id.favorites -> {
-                    Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this, R.string.favorites, Toast.LENGTH_SHORT).show()
                     true
                 }
                 R.id.watch_later -> {
-                    Toast.makeText(this, "Посмотреть позже", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this, R.string.watch_later, Toast.LENGTH_SHORT).show()
                     true
                 }
                 R.id.selections -> {
-                    Toast.makeText(this, "Подборки", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this, R.string.selections, Toast.LENGTH_SHORT).show()
                     true
                 }
                 else -> false
             }
         }
+
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.fragment_placeholder, HomeFragment())
+            .addToBackStack(null)
+            .commit()
+    }
+
+    fun launchDetailsFragment(film: Film) {
+        //Создаем "посылку"
+        val bundle = Bundle()
+        //Кладем наш фильм в "посылку"
+        bundle.putParcelable("film", film)
+        //Кладем фрагмент с деталями в перменную
+        val fragment = DetailsFragment()
+        //Прикрепляем нашу "посылку" к фрагменту
+        fragment.arguments = bundle
+
+        //Запускаем фрагмент
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_placeholder, fragment)
+            .addToBackStack(null)
+            .commit()
     }
 }
